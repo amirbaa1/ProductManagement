fcdbdfa1e6105f9716f0732069cc195da5147671
amirbaa1 <amir.baahklag@yahoo.com>
2023-06-29 00:25:04 +0330

    delete suppliers in app

--- suppliers/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Supplier
-
-
-@admin.register(Supplier)
-class AdminSupplier(admin.ModelAdmin):
-    list_display = ['name', 'contactInfo']
--- suppliers/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-
-class Supplier(models.Model):
-    SupplierId = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=50)
-    contactInfo = models.IntegerField()
