5e659a911b0a9608276c5c2af3bddbc808e25f88
amirbaa1 <amir.baahklag@yahoo.com>
2023-07-01 20:49:11 +0330

    clean code

--- pages/urls.py
+++ pages/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('about/', AboutPage.as_view(), name='about'),
     path('contact_us/', ContactUsPage.as_view(), name='contact_us'),
     path('panel/', login_required(Panel_Admin.as_view()), name='panel_admin'),
+
     path('panel/order_customer/', Panel_AdminOrder.as_view(), name='list_order_customer'),
     path('panel/order_customer/<int:order_id>/', View_Order.as_view(), name='Det_order'),
 
@@ -18,13 +19,16 @@ urlpatterns = [
     path('panel/add-inventory-city/', add_cityInventory.as_view(), name='add_city_inv'),
     path('panel/add-inventory-city/delete/<int:id>/', Delete_city_inventory.as_view(), name='delete-inv_city'),
     path('panel/inventory/delete/<int:id>/', Delete_Inventory.as_view(), name='delete-inv'),
+
     path('panel/supplier/', List_Supplier.as_view(), name='link_sup_list'),
     path('panel/supplier/delete/<int:id>/', Delete_Supplier.as_view(), name='delete_sup'),
     path('panel/supplier/add/', add_Supplier.as_view(), name='add_sup'),
     path('panel/supplier/update/<int:id>', Update_Supplier.as_view(), name='link_upd_supp'),
+
     path('panel/product-variations/', Product_view_list.as_view(), name='link_list_pro_v'),
     path('panel/product-variations/update/<int:id>', Update_Pro_v.as_view(), name='link_up_pro_v'),
     path('panel/product-variations/delete/<int:id>/', Delete_ProductVariation.as_view(), name='delete-pro_v'),
+
     path('panel/add-product-category/', add_category_product.as_view(), name='add_pro_cat'),
     path('panel/list-product-category/', List_cat_product.as_view(), name='link_list_pro_cat'),
     path('panel/product-category/delete/<int:id>/', Delete_cat_pro.as_view(), name='delete-pro_cat'),
